a317bd3c52411880064f23423a2cfc5a02f2a078
tmannazy <e.anene@hotmail.com>
2022-06-22 11:01:44 +0100

    Check pairs

--- assignments/is_string_pair_complete.py
+++ assignments/is_string_pair_complete.py
@@ -1,21 +1,48 @@
 open_char_lst = ['[', '{', '(', '<']
 close_char_lst = [']', '}', ')', '>']
 
+#
+# def is_char_complete(str_to_check):
+#     lst = []
+#     for x in str_to_check:
+#         if x in open_char_lst:
+#             lst.append(x)
+#         elif x in close_char_lst:
+#             get_index = close_char_lst.index(x)
+#             if len(lst) > 0:
+#                 if open_char_lst[get_index] == lst[len(lst) - 1]:
+#                     lst.pop()
+#     if len(lst) == 0:
+#         return True
+#     else:
+#         return False
+#
+#
+# print(is_char_complete("[{()}]"))
 
-def is_char_complete(str_to_check):
-    lst = []
-    for x in str_to_check:
-        if x in open_char_lst:
-            lst.append(x)
-        elif x in close_char_lst:
-            get_index = close_char_lst.index(x)
-            if len(lst) > 0:
-                if open_char_lst[get_index] == lst[len(lst) - 1]:
-                    lst.pop()
-    if len(lst) == 0:
-        return True
+
+def bracket_pair(brackets: str) -> bool:
+    if len(brackets) < 2 or len(brackets) % 2 != 0:
+        return False
+    stack = []
+
+    for bracket in brackets:
+        if bracket in "{([":
+            stack.append(bracket)
+        elif bracket in "])}":
+            peeked = stack[- 1]
+            if bracket == ")" and peeked == "(":
+                stack.pop()
+            elif bracket == "}" and peeked == "{":
+                stack.pop()
+            elif bracket == "]" and peeked == "[":
+                stack.pop()
+            return True
+        else:
+            return False
     else:
         return False
 
 
-print(is_char_complete("[{()}]"))
+
+print(bracket_pair("{}{(([]))}"))
--- assignments/rotate_list.py
+++ assignments/rotate_list.py
@@ -1,16 +1,28 @@
-def rotate_list(lst, rotation_num):
-    new_lst = []
-    for x in range(rotation_num):
-        if len(new_lst) == 0:
-            if type(lst[0]) is not list:
-                el = [lst[0]]
-                new_lst = lst[1:] + el
-        else:
-            if type(new_lst[0]) is not list:
-                new_el = [new_lst[0]]
-                new_lst = new_lst[1:] + new_el
+# def rotate_list(lst, rotation_num):
+#     new_lst = []
+#     for x in range(rotation_num):
+#         if len(new_lst) == 0:
+#             if type(lst[0]) is not list:
+#                 el = [lst[0]]
+#                 new_lst = lst[1:] + el
+#         else:
+#             if type(new_lst[0]) is not list:
+#                 new_el = [new_lst[0]]
+#                 new_lst = new_lst[1:] + new_el
+#
+#     return new_lst
+#
+#
+# print(rotate_list([1, 2, 3, 4, 5], 3))
 
-    return new_lst
+def rotate(list_to_rotate: list[int], rotate_by: int) -> list[int]:
+    rotate_by = rotate_by % len(list_to_rotate)
+    return list_to_rotate[rotate_by:] + list_to_rotate[:rotate_by]
 
 
-print(rotate_list([1, 2, 3, 4, 5], 3))
+# def rotate(lst, p):
+#     p = p % len(lst)
+#     return lst[p:] + lst[:p]
+
+
+print(rotate(["you", 5, 8, 9, 6, 3], 4))
